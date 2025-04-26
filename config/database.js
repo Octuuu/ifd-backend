3409ee5e63933a925d8c73bc903c9b62719b6776
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-04-26 16:43:57 -0300

    ..

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATOS_PUBLIC_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
