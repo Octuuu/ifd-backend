4880824b033308c085cf3693fb660d97a28af202
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-04-26 14:17:46 -0300

    ..

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());  // Habilitar CORS para permitir solicitudes desde otros dominios
+app.use(cors({
+  origin: 'https://ifd.vercel.app/', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+}));
 app.use(express.json());  // Middleware para parsear los cuerpos de las solicitudes como JSON
 
 // Sirve los archivos estáticos desde la carpeta 'uploads'
