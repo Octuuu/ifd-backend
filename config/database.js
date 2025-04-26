d7a0bab69bdd83f719228dc48ceebd634a786cf3
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-04-26 16:31:35 -0300

    ..

--- config/database.js
+++ config/database.js
@@ -1,15 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env['DATABASE_URL'], {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false 
     }
   },
-  logging: false 
+  logging: false
 });
 
 export default sequelize;
